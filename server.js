b89ae6a2ec8ef3871450e9262dc39f427bcb4509
gyuiru <gyuiru8@gmail.com>
2023-11-08 11:13:50 +0900

    feat: mongoDB接続
    色々なDBの中でMongoDBを選択、接続する
    
    #1

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, () => {
+const { MongoClient } = require('mongodb');
+
+let db
+const url = 'mongodb+srv://gyuiru:reol81776@cluster0.rqprmfn.mongodb.net/?retryWrites=true&w=majority'
+new MongoClient(url).connect().then((client)=>{
+  console.log('DB연결성공');
+  db = client.db('treefy');
+  app.listen(3000, () => {
     console.log('http://localhost:3000 에서 서버 실행중');
-})
+  });
+}).catch((err)=>{
+  console.log(err);
+});
 
 app.get('/', (req, res) => {
   res.send('home');
