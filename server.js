3f5866465a1270ebcf121db72fc8cc90cac2429d
gyuiru <gyuiru8@gmail.com>
2023-11-29 14:55:39 +0900

    feat: mysql接続
    RDBMSの勉強のためMySQLを選択、接続する
    
    #1

--- server.js
+++ server.js
@@ -4,19 +4,26 @@ const app = express();
 app.use(express.json());
 var cors = require('cors');
 app.use(cors());
+require('dotenv').config();
 
-const { MongoClient } = require('mongodb');
+const mysql = require('mysql2');
 
-let db
-const url = 'mongodb+srv://gyuiru:reol81776@cluster0.rqprmfn.mongodb.net/?retryWrites=true&w=majority'
-new MongoClient(url).connect().then((client)=>{
-  console.log('DB연결성공');
-  db = client.db('treefy');
-  app.listen(3000, () => {
-    console.log('http://localhost:3000 에서 서버 실행중');
-  });
-}).catch((err)=>{
-  console.log(err);
+const connection = mysql.createConnection({
+host : process.env.DB_HOST,  
+user : process.env.DB_USER,
+password : process.env.DB_PASSWORD,
+database : process.env.DB_NAME,
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('disconnected DB :' + err.stack);
+    return;
+  }
+  console.log('connected DB');
+  app.listen(process.env.PORT, () => {
+    console.log('connected server')
+  })
 });
 
 app.use(express.static(path.join(__dirname, '../treefy-fe/dist/index.html')));
@@ -25,6 +32,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../treefy-fe/dist/index.html'));
 });
 
-app.get('/', (req, res) => {
-  res.send('home');
-});
+// app.get('/', (req, res) => {
+//   res.send('home');
+// });
