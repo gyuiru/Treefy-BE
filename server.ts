750eb009cf1913e6b1b947afcb60b6a9b4f0796a
gyuiru <gyuiru8@gmail.com>
2023-12-06 08:48:00 +0900

    feat: postsデータをjoin componentに受け渡す
    Postsデータをjoin componentに受け渡す
    
    #2

--- server.ts
+++ server.ts
@@ -9,6 +9,7 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../../treefy-fe/dist'))); // Express 서버가 정적 파일들을 제공하도록 설정하는 코드
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 dotenv.config();
 
 const connection = mysql.createConnection({
@@ -37,7 +38,7 @@ app.post('/posts', (req: Request, res: Response) => {
   try {
     console.log("POST request received");
     console.log(req.body);
-    const postsInsertQuery = 'INSERT INTO posts (title, content, created_date) VALUES (?, ?, NOW())';
+    const postsInsertQuery = 'INSERT INTO posts (title, content, created_date) VALUES (?, ?, DATE(NOW()))';
     connection.query(postsInsertQuery, [req.body.title, req.body.content], (error, _results, _fields) => {
       if (error) {
         console.error('Error executing query: ' + error.stack);
@@ -50,5 +51,21 @@ app.post('/posts', (req: Request, res: Response) => {
   } catch (error) {
     console.error(error);
     res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/list', (_req: Request, res: Response) => {
+  try {
+    connection.query('SELECT * FROM posts', (error, results, _fields) => {
+      if (error) {
+        console.error('Error executing query: ' + error.stack);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+      console.log('결과 : ', results);
+      res.send(results);
+    })
+  } catch (error) {
+    console.error(error);
   }
 });
