b78b06e6142fa46ae4f641f440e66931c586d7e6
gyuiru <gyuiru8@gmail.com>
2023-12-05 10:20:59 +0900

    feat: postデーターを取得してDBに保存
    Postデーターを取得してDBに保存
    
    #2

--- server.ts
+++ server.ts
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../../treefy-fe/dist'))); // Express 서버가 정적 파일들을 제공하도록 설정하는 코드
+app.use(express.json());
 dotenv.config();
 
 const connection = mysql.createConnection({
@@ -30,4 +31,24 @@ connection.connect((err) => {
 
 app.get('/', (_req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../../treefy-fe/dist/index.html'));
+});
+
+app.post('/posts', (req: Request, res: Response) => {
+  try {
+    console.log("POST request received");
+    console.log(req.body);
+    const postsInsertQuery = 'INSERT INTO posts (title, content, created_date) VALUES (?, ?, NOW())';
+    connection.query(postsInsertQuery, [req.body.title, req.body.content], (error, _results, _fields) => {
+      if (error) {
+        console.error('Error executing query: ' + error.stack);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+      console.log('Data added to database');
+      res.status(201).send(req.body);
+    })
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
 });
