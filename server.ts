b9ff87944c543e7797cf654b744e896521510d83
gyuiru <gyuiru8@gmail.com>
2023-12-12 10:41:09 +0900

    feat: passport設定完了
    ユーザーのログインのためpassportを利用
    
    #8

--- server.ts
+++ server.ts
@@ -4,6 +4,9 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import mysql from 'mysql2/promise';
 import { RowDataPacket } from 'mysql2/promise';
+import session from 'express-session';
+import passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
 
 const app = express();
 app.use(express.json());
@@ -12,6 +15,16 @@ app.use(express.static(path.join(__dirname, '../../treefy-fe/dist'))); // Expres
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 dotenv.config();
+
+interface TotalPostsCount extends RowDataPacket {
+  totalPostsCount: number
+}
+
+interface UserInfo extends RowDataPacket {
+  username: string;
+  password: string;
+  nickname: string;
+}
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -22,7 +35,7 @@ const pool = mysql.createPool({
 
 // 연결 풀을 사용하여 데이터베이스에 연결하려는 시도를 한다
 pool.getConnection()
-  .then(connection => {
+.then(connection => {
     // 연결 성공 시 로그를 출력하고 연결을 풀에 반환한다
     console.log('connected DB');
     connection.release();
@@ -34,11 +47,34 @@ pool.getConnection()
     // 연결 실패 시 에러를 출력한다
     console.error('disconnected DB :' + err.stack);
     return;
-  });
+});
 
-interface totalPostsCount extends RowDataPacket {
-  totalPostsCount: number
-}
+app.use(passport.initialize());
+app.use(session({
+  secret: '암호화에 쓸 비번',
+  resave : false, // 유저가 서버로 요청할 때마다 세션 갱신할건지 (false가 일반적)
+  saveUninitialized : false // 로그인 안해도 세션 만들것인지 (false가 일반적)
+}))
+
+app.use(passport.session());
+
+passport.use(new LocalStrategy(async (username: string, password: string, done) => {
+  try {
+    const connection = await pool.getConnection();
+    const [results] = await connection.query<UserInfo[]>('SELECT * FROM treefy.`user` WHERE username = ?', [username]);
+    if (!results.length) {
+      return done(null, false, { message: '아이디 DB에 없음' })
+    }
+    if (results[0].password === password) {
+      return done(null, results[0]);
+    } else {
+      return done(null, false, { message: '비번불일치' });
+    }
+  } catch (error) {
+    console.log(error);
+    done(new Error('Internal Server Error'));
+  }
+}));
 
 app.get('/', (_req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../../treefy-fe/dist/index.html'));
@@ -95,7 +131,7 @@ app.get('/list', async (req: Request, res: Response) => {
 app.get('/list/count', async (_req: Request, res: Response) => {
   try {
     const connection = await pool.getConnection();
-    const [results] = await connection.query<totalPostsCount[]>('SELECT COUNT(*) as totalPostsCount FROM posts');
+    const [results] = await connection.query<TotalPostsCount[]>('SELECT COUNT(*) as totalPostsCount FROM posts');
     connection.release();
     console.log('전체 글 갯수 : ', results[0].totalPostsCount);
     res.send(results[0].totalPostsCount.toString());
@@ -166,4 +202,24 @@ app.post('/signup', async (req: Request, res: Response) => {
     console.log(error);
     res.status(500).send('Internal Server Error');
   }
+});
+
+app.post('/login', async (req: Request, res: Response, next) => {
+  console.log(req.body);
+  passport.authenticate('local', (error: Error | null, user: UserInfo | false, info: { [key: string]: string }) => {
+  try {
+      if (error) return res.status(500).json(error);
+      if (!user) return res.status(401).json(info && info.message ? info.message : '로그인 실패');
+      return req.logIn(user, (error) => {
+        if (error) {
+          res.status(500).send('로그인 중 에러 발생');
+          return next(error);
+        }
+        return res.send('로그인 성공');
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send('Internal Server Error');
+    }
+  })(req, res, next);
 });
